201c47974d5a933332c93df049118fbc0db58e54
Joseph Fuge <josephfuge1@gmail.com>
2024-02-10 21:52:42 -0700

    Add project files.

---
+++ Mission06_Fuge/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Fuge.Models;
+using System.Diagnostics;
+
+namespace Mission06_Fuge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnowJoel()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie newMovie)
+        {
+            return View(newMovie);
+        }
+    }
+}
---
+++ Mission06_Fuge/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace Mission06_Fuge.Models
+{
+    public class Movie
+    {
+        public int Id { get; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
